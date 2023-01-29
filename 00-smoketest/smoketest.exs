401fb5f90c8484f24196518375ae77ebe644752a
MikeBeller <bellermike@gmail.com>
2023-01-29 16:24:41 +0000

    add test

--- 00-smoketest/smoketest.exs
+++ 00-smoketest/smoketest.exs
@@ -17,11 +17,33 @@ defmodule Smoke do
       {:ok, data} ->
         :gen_tcp.send(sock, data)
         serve_echo(sock)
-      {:error, err} ->
-        IO.inspect(err)
+      {:error, :closed} ->
+          :gen_tcp.close(sock)
     end
   end
 end
 
-Smoke.listen(9999)
-System.no_halt(true)
+##### test code
+
+if System.argv() == ["--serve"] do
+  Smoke.listen(9999)
+  System.no_halt(true)
+else
+  ExUnit.start()
+end
+
+defmodule Smoke.SmokeTest do
+  use ExUnit.Case, async: true
+
+  test "smoke" do
+    {:ok, pid} = Task.start_link(fn -> Smoke.listen(9999) end)
+    {:ok, sock} = :gen_tcp.connect('localhost', 9999, [:binary, packet: :raw, active: false])
+    assert :ok = :gen_tcp.send(sock, "hello")
+    assert {:ok, "hello"} = :gen_tcp.recv(sock, 0)
+    assert :ok = :gen_tcp.send(sock, "world")
+    :ok = :gen_tcp.shutdown(sock, :write)
+    assert {:ok, "world"} = :gen_tcp.recv(sock, 0)
+    :ok = :gen_tcp.close(sock)
+    Process.exit(pid, :kill)
+  end
+end
