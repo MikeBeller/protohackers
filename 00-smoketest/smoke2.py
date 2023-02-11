b7322084815b7a6539d5a96127b4c0ffee74dd5e
MikeBeller <bellermike@gmail.com>
2023-02-11 01:34:47 +0000

    working smoke2 async with client and server

--- 00-smoketest/smoke2.py
+++ 00-smoketest/smoke2.py
@@ -5,30 +5,36 @@ async def server(port):
   server = await asyncio.start_server(echo_handler, '127.0.0.1', port)
   print('serving on port', server.sockets[0].getsockname()[1])
   async with server:
-    await server.wait_closed()
+    await server.serve_forever()
     
-async def echo_handler(conn):
+async def echo_handler(reader, writer):
   "echo handler"
   while True:
-    data = await conn.recv(1024)
+    data = await reader.read(1024)
     if not data:
       break
-    await conn.send(data)
-  conn.close()
-
-async def test(port):
-  stask = asyncio.create_task(server(port))
-  ctask = asyncio.create_task(client(port))
-  await asyncio.wait([ ctask])
-  stask.cancel()
-  
+    writer.write(data)
+    await writer.drain()
+  writer.close()
 
 async def client(port):
   "async tcp client"
   rd,wr = await asyncio.open_connection('127.0.0.1', port)
-  await wr.send(b'hello')
-  data = await rd.recv(1024)
+  wr.write(b'hello')
+  await wr.drain()
+  data = await rd.read(1024)
   assert data == b'hello'
+  wr.close()
+  await wr.wait_closed()
+  print("OK")
+
+async def test(port):
+  stask = asyncio.create_task(server(port))
+  await asyncio.sleep(0.1)
+  ctask = asyncio.create_task(client(port))
+  await asyncio.wait([ ctask])
+  stask.cancel()
+  await asyncio.wait([ stask])
 
 if __name__ == "__main__":
    asyncio.run(test(8080))
