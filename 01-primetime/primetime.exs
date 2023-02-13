4da81ff4cdaf9844731d0b4686f9710dedff80b4
MikeBeller <bellermike@gmail.com>
2023-02-13 01:51:08 +0000

    putatively working version of primetime in elixir

--- 01-primetime/primetime.exs
+++ 01-primetime/primetime.exs
@@ -1,6 +1,10 @@
-Mix.install({:jason, "~> 1.0"})
+Mix.install([{:jason, "~> 1.0"}])
 
-defmodule Smoke do
+defmodule PrimeTime do
+  def is_prime(n) do
+    MillerRabin.miller_rabin?(n, 10)
+  end
+
   def listen(port) do
     {:ok, sock} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
     IO.puts "Accepting connections on port #{port}"
@@ -17,33 +21,66 @@ defmodule Smoke do
     Jason.encode!(%{"error" => "invalid"}) <> "\n"
   end
 
-  # need prime algo
-  defp prime(num) do
-    false
-  end
-
   defp primetime(data) do
+    IO.puts "got data #{inspect data}"
     case Jason.decode(data) do
-      {:ok, %{"method" => "isPrime", "number" => num when is_integer(num)}} ->
-        Jason.encode!(%{"method" => "isPrime", "prime" => prime(num)}) <> "\n"
-      {:ok, %{"method" => "isPrime", "number" => num when is_float(num)}} ->
+      {:ok, %{"method" => "isPrime", "number" => num }} when is_integer(num) ->
+        Jason.encode!(%{"method" => "isPrime", "prime" => is_prime(num)}) <> "\n"
+      {:ok, %{"method" => "isPrime", "number" => num}}  when is_float(num) ->
         Jason.encode!(%{"method" => "isPrime", "prime" => false}) <> "\n"
       _ -> error_json()
     end
   end
 
-  defp serve_echo(sock) do
+  defp serve_prime(sock) do
     IO.puts "new client #{inspect sock}"
     case :gen_tcp.recv(sock, 0) do
       {:ok, data} ->
         resp = primetime(data)
         :gen_tcp.send(sock, resp)
-        serve_echo(sock)
+        serve_prime(sock)
       {:error, err} ->
         IO.inspect(err)
     end
   end
 end
 
-Smoke.listen(9999)
+defmodule MillerRabin do
+  def modular_exp( x, y, mod ) do
+     with [ _ | bits ] = Integer.digits( y, 2 ) do
+          Enum.reduce bits, x, fn( bit, acc ) -> acc * acc |> ( &( if bit == 1, do: &1 * x, else: &1 ) ).() |> rem( mod ) end
+     end
+  end
+
+  def miller_rabin( d, s ) when rem( d, 2 ) == 0, do: { s, d }
+  def miller_rabin( d, s ), do: miller_rabin( div( d, 2 ), s + 1 )
+
+  def miller_rabin?( n, g ) do
+       { s, d } = miller_rabin( n - 1, 0 )
+       miller_rabin( n, g, s, d )
+  end
+
+  def miller_rabin( n, 0, _, _ ), do: true
+  def miller_rabin( n, g, s, d ) do
+    a = 1 + :rand.uniform( n - 3 )
+    x = modular_exp( a, d, n )
+    if x == 1 or x == n - 1 do
+      miller_rabin( n, g - 1, s, d )
+    else
+      if miller_rabin( n, x, s - 1) == True, do: miller_rabin( n, g - 1, s, d ), else: false
+    end
+  end
+
+  def miller_rabin( n, x, r ) when r <= 0, do: false
+  def miller_rabin( n, x, r ) do
+    x = modular_exp( x, 2, n )
+    unless x == 1 do
+      unless x == n - 1, do: miller_rabin( n, x, r - 1 ), else: true
+    else
+      False
+    end
+  end
+end
+
+PrimeTime.listen(9999)
 Pocess.sleep(:infinity)
---
+++ 01-primetime/primetime_tester.exs
@@ -0,0 +1,57 @@
+Mix.install([{:jason, "~> 1.0"}])
+
+ExUnit.start()
+
+defmodule Primetime.PrimetimeTest do
+  use ExUnit.Case
+
+  defp connect(port) do
+    {:ok, sock} = :gen_tcp.connect('localhost', port,
+      [:binary, active: false, packet: :line, reuseaddr: true])
+    sock
+  end
+
+  @port 9999
+
+  test "primetime_7" do
+    sock = connect(@port)
+    msg = Jason.encode!(%{"method" => "isPrime", "number" => 7}) <> "\n"
+    assert :ok = :gen_tcp.send(sock, msg)
+    assert {:ok, resp} = :gen_tcp.recv(sock, 0, 3000)
+    assert Jason.decode!(resp) == %{"method" => "isPrime", "prime" => true}
+    :ok = :gen_tcp.close(sock)
+  end
+
+  test "primetime_81" do
+    sock = connect(@port)
+    msg = Jason.encode!(%{"method" => "isPrime", "number" => 81}) <> "\n"
+    assert :ok = :gen_tcp.send(sock, msg)
+    assert {:ok, resp} = :gen_tcp.recv(sock, 0, 3000)
+    assert Jason.decode!(resp) == %{"method" => "isPrime", "prime" => false}
+    :ok = :gen_tcp.close(sock)
+  end
+
+  # set qs to a list of 5 numbers some of which are prime and some of which are not
+  @qs [7, 81, 13, 14, 15]
+  # set as to true or false depending on whether the corresponding number in qs is prime
+  @as [true, false, true, false, false]
+
+  test "supports 5 simultaneous connections" do
+    socks = Enum.map(1..5, fn _ -> connect(@port) end)
+    Enum.each(Enum.zip(socks, @qs), fn {sock, q} ->
+      assert :ok = :gen_tcp.send(sock, Jason.encode!(%{"method" => "isPrime", "number" => q}) <> "\n")
+    end)
+
+    Enum.zip(socks, @as)
+    |> Enum.reverse()
+    |> Enum.each(fn {sock, a} ->
+      assert {:ok, resp} = :gen_tcp.recv(sock, 0, 3000)
+      assert Jason.decode!(resp) == %{"method" => "isPrime", "prime" => a}
+    end)
+
+    Enum.each(socks, fn sock ->
+      :ok = :gen_tcp.close(sock)
+    end)
+  end
+
+end
