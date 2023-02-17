b7df5d0515a5c307a7446700a82a75910e4110f6
MikeBeller <bellermike@gmail.com>
2023-02-17 01:59:49 +0000

    Add auto-running of server in test

--- 02-means/means_test.exs
+++ 02-means/means_test.exs
@@ -1,4 +1,4 @@
-Mix.install([{:jason, "~> 1.0"}])
+Mix.install([{:porcelain, "~> 2.0"}])
 
 ExUnit.start()
 
@@ -12,7 +12,6 @@ defmodule Means.MeansTest do
   end
 
   @port 9999
-
   @test_now 1676562865
 
   defp insert_msg(ts, price) do
@@ -21,6 +20,19 @@ defmodule Means.MeansTest do
 
   defp query_msg(min_ts, max_ts) do
     <<?Q, min_ts::size(32), max_ts::size(32)>>
+  end
+
+  setup_all do
+    pid = Porcelain.spawn("elixir", ["means.exs"])
+    IO.puts "Started #{inspect(pid)}"
+    Process.sleep(1000)
+    on_exit(fn ->
+      IO.puts "Stopping #{inspect(pid)}"
+      Porcelain.Process.stop(pid)
+      Porcelain.Process.await(pid)
+      IO.puts("Stopped #{inspect(pid)}")
+    end)
+    {:ok, pid: pid}
   end
 
   test "handles one item" do
@@ -40,7 +52,7 @@ defmodule Means.MeansTest do
     :ok = :gen_tcp.close(sock)
   end
 
-  test "handles multiple itesm" do
+  test "handles multiple items" do
     sock = connect(@port)
     assert :ok = :gen_tcp.send(sock, insert_msg(@test_now, 100))
     assert :ok = :gen_tcp.send(sock, insert_msg(@test_now+10, 200))
@@ -50,6 +62,15 @@ defmodule Means.MeansTest do
     assert :ok = :gen_tcp.send(sock, insert_msg(@test_now+15, 400))
     assert :ok = :gen_tcp.send(sock, query_msg(@test_now, @test_now+21))
     assert {:ok, <<250::size(32)>>} = :gen_tcp.recv(sock, 4, 3000)
+    :ok = :gen_tcp.close(sock)
+  end
+
+  test "handles negative price" do
+    sock = connect(@port)
+    assert :ok = :gen_tcp.send(sock, insert_msg(@test_now, -100))
+    assert :ok = :gen_tcp.send(sock, query_msg(@test_now-10, @test_now+10))
+    assert {:ok, resp} = :gen_tcp.recv(sock, 4, 3000)
+    assert <<-100::signed-integer-size(32)>> = resp
     :ok = :gen_tcp.close(sock)
   end
 
